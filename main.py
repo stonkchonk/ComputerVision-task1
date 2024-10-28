555f1f5f5719cf724a3509b1fd234064352169e7
stonkchonk <heilfred98@gmail.com>
2024-10-28 11:00:44 +0100

    improve script slightly

--- main.py
+++ main.py
@@ -1,25 +1,33 @@
 import cv2
 import cv2.aruco as aruco
 import numpy as np
+from os import listdir
 
-input_files = [
-    '20221115_113319.jpg',
-    '20221115_113328.jpg',
-    '20221115_113340.jpg',
-    '20221115_113346.jpg',
-    '20221115_113356.jpg',
-    '20221115_113401.jpg',
-    '20221115_113412.jpg',
-    '20221115_113424.jpg',
-    '20221115_113437.jpg',
-    '20221115_113440.jpg',
-    '20221115_113653.jpg',
-]
+
+def load_image(path_to_file: str) -> cv2.typing.MatLike:
+    return cv2.imread(path_to_file, cv2.IMREAD_COLOR)
+
+
+def add_two_images(first: cv2.typing.MatLike, second: cv2.typing.MatLike):
+    assert first.shape == second.shape
+    original_shape = first.shape
+    first_flat = first.flat
+    second_flat = second.flat
+    for idx, val in enumerate(second_flat):
+        if val > 0:
+            first_flat[idx] = val
+    return np.reshape(first_flat, original_shape)
+
+
+input_files = listdir("images/")
+poster = load_image("loadbearingposter.png")
+aruco_diameter = 64
+poster_height, poster_width, _ = poster.shape
 
 for file in input_files:
 
-    img = cv2.imread(f"images/{file}", cv2.IMREAD_COLOR)  # images/20221115_113319.jpg
-    print("reading file " + file + "\n")
+    img = load_image(f"images/{file}")
+    print("reading file " + file)
 
     dictionary = cv2.aruco.getPredefinedDictionary(aruco.DICT_6X6_50)
 
@@ -28,29 +36,27 @@ for file in input_files:
     rejected_candidates = np.zeros(0)
     detector_params = cv2.aruco.DetectorParameters()
     detector = cv2.aruco.ArucoDetector(dictionary, detector_params)
-    try:
-        aruco_detections = detector.detectMarkers(img, marker_corners, marker_ids, rejected_candidates)
-    except:
+    aruco_detections = detector.detectMarkers(img, marker_corners, marker_ids, rejected_candidates)
+    if aruco_detections[1] is None:
         print(f"No ArUco detected in file: {file}")
         continue
 
-    print(aruco_detections[0][0][0])
-    print("---")
-    print(aruco_detections[1])
-    print("---")
-
     height, width, _ = img.shape
-    print(height, width)
     detected_corners = aruco_detections[0][0][0]
-    actual_corners = np.float32([[224, 224], [288, 224], [288, 288], [224, 288]])
 
-    transformable_image = cv2.imread("loadbearingposter.png", cv2.IMREAD_COLOR)
+    actual_corners = np.float32([
+        [poster_height / 2 - aruco_diameter / 2, poster_width / 2 - aruco_diameter / 2],
+        [poster_height / 2 + aruco_diameter / 2, poster_width / 2 - aruco_diameter / 2],
+        [poster_height / 2 + aruco_diameter / 2, poster_width / 2 + aruco_diameter / 2],
+        [poster_height / 2 - aruco_diameter / 2, poster_width / 2 + aruco_diameter/2]
+    ])
+
     M = cv2.getPerspectiveTransform(actual_corners, detected_corners)
-    print("M:", M)
-    warped = cv2.warpPerspective(transformable_image, M, (width, height))
+    warped = cv2.warpPerspective(poster, M, (width, height))
 
-    combined = cv2.addWeighted(img, 1, warped, 1, 0)
+    combined = cv2.addWeighted(img, 1, warped, 1, 0) #add_two_images(img, warped)
+
+    for corner in detected_corners:
+        combined[int(corner[1])][int(corner[0])] = [0, 0, 255]
+
     cv2.imwrite(f'output/combined_{file}', combined)
-
-
-
